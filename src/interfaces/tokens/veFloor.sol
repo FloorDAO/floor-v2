62b6f0d26b6476ca9ee9bd6395d6ced769c7d451
Tom Wade <twade7@gmail.com>
2022-10-26 16:23:10 +0100

    Add veFloor and staking

---
+++ src/interfaces/tokens/veFloor.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import './ERC20.sol';
+
+
+/**
+ * https://snowtrace.io/address/0x3cabf341943Bc8466245e4d6F1ae0f8D071a1456#code
+ */
+
+interface IVeFLOOR is IERC20 {
+
+  /**
+   * Gets the address of the current staking contract attached to the ERC20.
+   */
+  function stakingContract() external view returns (address);
+
+  /**
+   * Creates `_amount` token to `_to`. Must only be called by the owner
+   */
+  function mint(address to_, uint256 amount_) external;
+
+  /**
+   * Destroys `_amount` tokens from `_from`. Callable only by the owner
+   */
+  function burn(address _from, uint _amount) external;
+
+  /**
+   * Sets the address of the staking contract that this token updates.
+   */
+  function setStakingContract(address _contractAddr) external;
+
+  /**
+   * Will need to implement the `_afterTokenOperation` ERC20 function so that whenever
+   * tokens are minted or burned, we
+   *
+   * We will also want to ensure that all other operations are reverted, so that the
+   * token cannot be transferred or manipulated.
+   */
+
+   function _afterTokenOperation(address _account, uint256 _newBalance) internal;
+
+}
--- src/interfaces/voting/VoteStaking.sol
+++ src/interfaces/voting/VoteStaking.sol
@@ -17,17 +17,93 @@ pragma solidity ^0.8.0;
  *
  *  - Increased voting power
  *  - Locked FLOOR holders receive yield from the veFLOOR gauge
+ *
+ * https://snowtrace.io/address/0x1bf56b7c132b5cc920236ae629c8a93d9e7831e7#code
  */
 interface IVoteStaking {
 
     /**
-     * Wraps our users Floor into veFloor.
+     * Sent when a user claims veFloor.
      */
-    function stake(address _to, uint256 _amount) external returns (uint);
+    event Claim(address indexed user, uint256 amount);
 
     /**
-     * Unwraps a users veFloor to Floor.
+     * Sent when the user stakes Floor for veFloor.
      */
-    function unstake(address _to, uint256 _amount) external returns (uint);
+    event Staked(address indexed user, uint256 amount);
+
+    /**
+     * Sent when the user unstakes veFloor to Floor.
+     */
+    event Unstaked(address indexed user, uint256 withdrawAmount, uint256 burnAmount);
+
+    /**
+     * The following events are sent when then owner updates any of the reward
+     * variables that determine the veFloor curve.
+     */
+    event UpdateMaxCapPct(address indexed user, uint256 maxCapPct);
+    event UpdateSpeedUpThreshold(address indexed user, uint256 speedUpThreshold);
+    event UpdateVeFloorPerSharePerSec(address indexed user, uint256 veFloorPerSharePerSec);
+
+    /**
+     * Sent when the reward variables are updated.
+     */
+    event UpdateRewardVars(uint256 lastRewardTimestamp, uint256 accVeFloorPerShare);
+
+    /**
+     * Deposits Floor to start staking for veFloor. Note that any pending veFloor
+     * should also be claimed in the process.
+     */
+    function stake(uint _amount) external returns (uint);
+
+    /**
+     * Withdraw staked Floor. Unstaking any amount of Floor means that the account
+     * will lose all current veFloor.
+     */
+    function unstake(uint _amount) external returns (uint);
+
+    /**
+     * Claim any pending veFloor for the user.
+     */
+    function claim(address _user) external;
+
+    /**
+     * Get the pending amount of veFloor for a given user.
+     */
+    function claimAvailable(address _user) external returns (uint);
+
+    /**
+     * Update global reward variables to calculate the accumulated veFloor per share.
+     */
+    function updateRewardVars() public;
+
+    /* --- */
+
+    /**
+     * Sets the max cap percentage; used to determine veFloor gain.
+     *
+     * The maximum limit of veFloor user can have as percentage points of staked
+     * Floor. For example, if user has `n` Floor staked, they can own a maximum of
+     * `n * maxCapPct / 100` veFloor.
+     */
+    function setMaxCapPct(uint _maxCapPct) external;
+
+    /**
+     * Sets the veFloor per share, per second; used to determine veFloor gain.
+     */
+    function setVeFloorPerSharePerSec(uint _veJoePerSharePerSec) external;
+
+    /**
+     * Sets the speed up threshold that determines the share curve; used to determine
+     * veFloor gain. The length of time a user receives speed up benefits.
+     *
+     * Percentage of user's current staked Floor user has to deposit in order to start
+     * receiving speed up benefits, in parts per 100.
+     *
+     * Specifically, user has to deposit at least `speedUpThreshold/100 * userStakedJoe`
+     * FLOOR. The only exception is the user will also receive speed up benefits if they
+     * are depositing with zero balance.
+     */
+    function setSpeedUpThreshold(uint _speedUpThreshold) external;
 
 }
