717fee72eaf2ffd1201c56bd49ab3adde39fa3cd
Tom Wade <twade7@gmail.com>
2022-11-29 23:30:02 +0000

    Add hardhat integration alongside Foundry

---
+++ hardhat.config.js
@@ -0,0 +1,43 @@
+/** @type import('hardhat/config').HardhatUserConfig */
+var fs = require('fs');
+require('hardhat-preprocessor');
+
+function getRemappings() {
+  return fs
+    .readFileSync("remappings.txt", "utf8")
+    .split("\n")
+    .filter(Boolean) // remove empty lines
+    .map((line) => line.trim().split("="));
+}
+
+module.exports = {
+  solidity: {
+    version: '0.8.17',
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  preprocess: {
+    eachLine: (hre) => ({
+      transform: (line) => {
+        if (line.match(/^\s*import /i)) {
+          for (let [from, to] of getRemappings()) {
+            if (line.includes(from)) {
+              line = line.replace(from, to);
+              break;
+            }
+          }
+        }
+        return line;
+      },
+    }),
+  },
+  paths: {
+    cache: './cache_hardhat',
+    sources: './src',
+    tests: './test/hardhat'
+  },
+};
