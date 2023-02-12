841df2f20a816638727c698498c131c17b659051
Tom Wade <twade7@gmail.com>
2023-02-12 23:58:33 +0000

    🐮🧹

---
+++ src/contracts/actions/sweepers/CowSwap.sol
@@ -0,0 +1,134 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import { IERC1271 } from "@openzeppelin/contracts/interfaces/IERC1271.sol";
+
+import { ICoWSwapSettlement } from "../../../interfaces/cowswap/CoWSwapSettlement.sol";
+import { GPv2Order } from "../../../interfaces/cowswap/GPv2Order.sol";
+import { ICoWSwapOnchainOrders } from "../../../interfaces/cowswap/CoWSwapOnchainOrders.sol";
+import { IWETH } from '../../../interfaces/tokens/WETH.sol';
+
+/// https://github.com/nlordell/dappcon-2022-smart-orders
+contract CowSwapSweeper is ICoWSwapOnchainOrders {
+
+    using GPv2Order for *;
+
+    struct Data {
+        IERC20 sellToken;
+        IERC20 buyToken;
+        address receiver;
+        uint256 sellAmount;
+        uint256 buyAmount;
+        uint32 validFrom;
+        uint32 validTo;
+        uint256 feeAmount;
+        bytes meta;
+    }
+
+    bytes32 constant public APP_DATA = keccak256('smart orders are cool');
+
+    ICoWSwapSettlement immutable public settlement;
+    bytes32 immutable public domainSeparator;
+
+    IWETH immutable public weth;
+
+    address immutable public treasury;
+
+    constructor(address settlement_, address weth_, address treasury_) {
+        settlement = ICoWSwapSettlement(settlement_);
+        domainSeparator = settlement.domainSeparator();
+        treasury = treasury_;
+        weth = IWETH(weth_);
+    }
+
+    function execute(address[] memory collections, uint[] memory amounts) public payable returns (bytes memory orderUid) {
+        // Wrap out msg.value into WETH
+        weth.deposit{value: msg.value}();
+
+        // Loop through our collections
+        for (uint i; i < collections.length; ++i) {
+            GPv2Order.Data memory order = GPv2Order.Data({
+                sellToken: weth,
+                buyToken: IERC20(collections[i]),
+                receiver: treasury,
+                sellAmount: amounts[i],
+                buyAmount: 0,
+                validTo: uint32(block.timestamp + 3600),
+                appData: APP_DATA,
+                feeAmount: 0,
+                kind: GPv2Order.KIND_SELL,
+                partiallyFillable: false,
+                sellTokenBalance: GPv2Order.BALANCE_ERC20,
+                buyTokenBalance: GPv2Order.BALANCE_ERC20
+            });
+
+            bytes32 orderHash = order.hash(domainSeparator);
+
+            GATOrder instance = new GATOrder{salt: bytes32('salt')}(
+                address(this),
+                order.sellToken,
+                uint32(block.timestamp),
+                orderHash,
+                settlement
+            );
+
+            order.sellToken.transferFrom(
+                address(this),
+                address(instance),
+                order.sellAmount + order.feeAmount
+            );
+
+            OnchainSignature memory signature = OnchainSignature({
+                scheme: OnchainSigningScheme.Eip1271,
+                data: hex""
+            });
+
+            emit OrderPlacement(address(instance), order, signature, ''); // TODO: 4th param is meta?
+
+            orderUid = new bytes(GPv2Order.UID_LENGTH);
+            orderUid.packOrderUidParams(orderHash, address(instance), order.validTo);
+        }
+    }
+
+}
+
+contract GATOrder is IERC1271 {
+
+    address immutable public owner;
+    IERC20 immutable public sellToken;
+    uint32 immutable public validFrom;
+
+    bytes32 public orderHash;
+
+    constructor(
+        address owner_,
+        IERC20 sellToken_,
+        uint32 validFrom_,
+        bytes32 orderHash_,
+        ICoWSwapSettlement settlement
+    ) {
+        owner = owner_;
+        sellToken = sellToken_;
+        validFrom = validFrom_;
+        orderHash = orderHash_;
+
+        sellToken_.approve(settlement.vaultRelayer(), type(uint256).max);
+    }
+
+    function isValidSignature(
+        bytes32 hash,
+        bytes calldata
+    ) external view returns (bytes4 magicValue) {
+        require(hash == orderHash, "invalid order");
+        require(block.timestamp >= validFrom, "not mature");
+        magicValue = 0x1626ba7e;  // ERC1271_MAGIC_VALUE
+    }
+
+    function cancel() public {
+        require(msg.sender == owner, "not the owner");
+        orderHash = bytes32(0);
+        sellToken.transfer(owner, sellToken.balanceOf(address(this)));
+    }
+}
---
+++ test/actions/sweepers/CowSwap.t.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+import {CowSwapSweeper} from '../../../src/contracts/actions/sweepers/CowSwap.sol';
+
+import {FloorTest} from '../../utilities/Environments.sol';
+
+contract CowSwapSweepTest is FloorTest {
+    // ..
+    address internal constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+
+    // ..
+    address internal constant SETTLEMENT_CONTRACT = 0x9008D19f58AAbD9eD0D60971565AA8510560ab41;
+
+    // Store our mainnet fork information
+    uint internal constant BLOCK_NUMBER = 16_134_863;
+
+    // Store our action contract
+    CowSwapSweeper action;
+
+    constructor() forkBlock(BLOCK_NUMBER) {
+        // Send this address as the {Treasury} parameter so we can see what comes back
+        action = new CowSwapSweeper(SETTLEMENT_CONTRACT, WETH, address(this));
+    }
+
+    /**
+     * Confirm that we can swap an approved amount of token with sufficient balance
+     * and receive back the expected amount of WETH into the {Treasury}.
+     */
+    function test_CanSweep() public {
+        address[] memory collections = new address[](2);
+        collections[0] = 0x269616D549D7e8Eaa82DFb17028d0B212D11232A;  // PUNK
+        collections[1] = 0xAbeA7663c472648d674bd3403D94C858dFeEF728;  // PUDGY
+
+        uint[] memory amounts = new uint[](2);
+        amounts[0] = 10 ether;
+        amounts[1] = 5 ether;
+
+        // Action our trade
+        bytes memory orderUid = action.execute{value: 15 ether}(collections, amounts);
+
+        // The action should now be added into the pool, with a UID returned for the
+        // order. As we have queried on a specific block, with specific data, we should
+        // be able to assert the specific order UID.
+        assertEq(orderUid, hex'341706193578f583c8ce8f3c715b01a9c044f8fd4eca7dc4f9bb5db184aa9e8482cf76d9b692a66a0dc5f8e0e4b65ca8451a649c6390f4eb');
+    }
+
+}
