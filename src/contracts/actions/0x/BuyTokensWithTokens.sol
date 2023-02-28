b0c4378f036bc5a6db9aeb7e84ce376546232c40
Tom Wade <twade7@gmail.com>
2023-02-28 11:55:01 +0000

    Finish 0x action

--- src/contracts/actions/0x/BuyTokensWithTokens.sol
+++ src/contracts/actions/0x/BuyTokensWithTokens.sol
@@ -4,12 +4,16 @@ pragma solidity ^0.8.0;
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-import {IAction} from '../../../interfaces/actions/Action.sol';
+import {IAction} from '@floor-interfaces/actions/Action.sol';
+import {IWETH} from '@floor-interfaces/tokens/WETH.sol';
 
 /**
  * @notice Buy tokens on 0x using another token.
  */
 contract BuyTokensWithTokens is IAction {
+
+    address internal constant ETH  = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
+    address internal constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
 
     /// Internal store of desired 0x contract
     address public immutable swapTarget;
@@ -42,26 +46,40 @@ contract BuyTokensWithTokens is IAction {
      * ..
      */
     function execute(bytes calldata _request) public payable returns (uint received_) {
-        // Unpack the request bytes data into our struct
-        ActionRequest memory request = abi.decode(_request, (ActionRequest));
+        // Unpack the request bytes data into individual variables, as mapping it directly
+        // to the struct is buggy (due to bytes memory -> storage?).
+        (address _sellToken, address _buyToken, bytes memory swapCallData) = abi.decode(_request, (address, address, bytes));
+
+        if (_sellToken != ETH) {
+            // Transfer our allowance of the `sellToken` from the {Treasury} so that
+            // we can handle any amount request. We later transfer dust back if left.
+            IERC20(_sellToken).transferFrom(treasury, address(this), IERC20(_sellToken).allowance(treasury, address(this)));
+        }
+        else {
+            IWETH(WETH).deposit{value: msg.value}();
+            _sellToken = WETH;
+        }
+
+        _buyToken = (_buyToken == ETH) ? WETH : _buyToken;
 
         // Set up our token references
-        IERC20 buyToken = IERC20(request.buyToken);
-        IERC20 sellToken = IERC20(request.sellToken);
+        IERC20 buyToken = IERC20(_buyToken);
+        IERC20 sellToken = IERC20(_sellToken);
 
         // Track our balance of the buyToken to determine how much we've bought.
         uint256 startAmountBuy = buyToken.balanceOf(address(this));
-        uint256 startAmountSell = sellToken.balanceOf(address(this));
 
         // Transfer our allowance of the `sellToken` from the {Treasury} so that
         // we can handle any amount request. We later transfer dust back if left.
-        sellToken.transferFrom(treasury, address(this), sellToken.allowance(treasury, address(this)));
+        if (_sellToken != WETH) {
+            sellToken.transferFrom(treasury, address(this), sellToken.allowance(treasury, address(this)));
+        }
 
         // Give `swapTarget` an allowance to spend this contract's `sellToken`.
         require(sellToken.approve(swapTarget, sellToken.balanceOf(address(this))), 'Unable to approve contract');
 
         // Call the encoded swap function call on the contract at `swapTarget`
-        (bool success,) = swapTarget.call(request.swapCallData);
+        (bool success,) = swapTarget.call(swapCallData);
         require(success, 'SWAP_CALL_FAILED');
 
         // Use our current buyToken balance to determine how much we've bought.
@@ -71,7 +89,7 @@ contract BuyTokensWithTokens is IAction {
         buyToken.transfer(treasury, received_);
 
         // If we still hold any `sellToken` then we can return this to the {Treasury} too
-        uint sellTokenDust = startAmountSell - sellToken.balanceOf(address(this));
+        uint sellTokenDust = sellToken.balanceOf(address(this));
         if (sellTokenDust != 0) {
             sellToken.transfer(treasury, sellTokenDust);
         }
