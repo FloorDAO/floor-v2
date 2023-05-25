bdbb97dc45873789f3656007764eea94565fcedb
Tom Wade <twade7@gmail.com>
2023-05-25 16:09:59 +0100

    Add trigger test

--- src/contracts/triggers/StoreEpochCollectionVotes.sol
+++ src/contracts/triggers/StoreEpochCollectionVotes.sol
@@ -32,7 +32,7 @@ contract StoreEpochCollectionVotesTrigger is EpochManaged, IEpochEndTriggered {
     ISweepWars public immutable sweepWars;
 
     /// Store a mapping of epoch to snapshot results
-    mapping(uint => EpochSnapshot) public epochSnapshots;
+    mapping(uint => EpochSnapshot) internal epochSnapshots;
 
     /**
      * Sets our internal contracts.
@@ -68,6 +68,13 @@ contract StoreEpochCollectionVotesTrigger is EpochManaged, IEpochEndTriggered {
 
         // Store our epoch snapshots
         epochSnapshots[epoch] = EpochSnapshot(epoch, collectionAddrs, collectionVotes);
+    }
+
+    /**
+     * Public function to get epoch snapshot data.
+     */
+    function epochSnapshot(uint epoch) external view returns (address[] memory, int[] memory) {
+        return (epochSnapshots[epoch].collections, epochSnapshots[epoch].votes);
     }
 
 }
