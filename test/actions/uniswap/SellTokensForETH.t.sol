518f9386c5ce57680da38640dfbad069d6e5df0b
Tom Wade <twade7@gmail.com>
2023-06-07 11:51:44 +0100

    Update to use UniversalRouter

--- test/actions/uniswap/SellTokensForETH.t.sol
+++ test/actions/uniswap/SellTokensForETH.t.sol
@@ -10,16 +10,19 @@ import {FloorTest} from '../../utilities/Environments.sol';
 
 contract UniswapSellTokensForETHTest is FloorTest {
     // Set up our Router02 address
-    address internal constant UNISWAP_ROUTER = 0xE592427A0AEce92De3Edee1F18E0157C05861564;
+    // address internal constant UNISWAP_ROUTER = 0xE592427A0AEce92De3Edee1F18E0157C05861564;
+    // address internal constant UNISWAP_ROUTER = 0xEf1c6E67703c7BD7107eed8303Fbe6EC2554BF6B;
+    address internal constant UNISWAP_ROUTER = 0x3fC91A3afd70395Cd496C647d5a6CC9D4B2b7FAD;
 
-    // We will be using USDC as our base token
+    // We will be using USDC as our base token and WETH as the received token
     address internal constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+    address internal constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
 
     // The pool fee for USDC/ETH is 0.05%
     uint24 internal constant USDC_FEE = 500;
 
     /// Store our mainnet fork information
-    uint internal constant BLOCK_NUMBER = 16_016_064;
+    uint internal constant BLOCK_NUMBER = 17_427_661;
 
     // Store our action contract
     UniswapSellTokensForETH action;
@@ -34,17 +37,45 @@ contract UniswapSellTokensForETHTest is FloorTest {
         // Set up our Treasury. In this test we will just use an account that
         // we know has the tokens that we need. This test will need to be updated
         // when our {Treasury} contract is completed.
-        treasury = 0x15abb66bA754F05cBC0165A64A11cDed1543dE48;
+        treasury = 0x171cda359aa49E46Dec45F375ad6c256fdFBD420;
 
         // Set up a floor migration contract
         action = new UniswapSellTokensForETH(UNISWAP_ROUTER);
+    }
+
+    function test_CanSwapToken() public {
+        vm.startPrank(treasury);
+
+        // Approve our entire USDC balance
+        ERC20(USDC).approve(address(action), type(uint256).max);
+
+        // Confirm our starting balances
+        assertEq(ERC20(USDC).balanceOf(treasury), 76980271562891);
+        assertEq(ERC20(WETH).balanceOf(treasury), 0);
+
+        // Action our trade
+        action.execute(
+            abi.encode(
+                address(USDC), // token0
+                uint24(USDC_FEE), // fee
+                uint(1000000000), // amountIn
+                uint(1), // amountOutMinimum
+                uint(block.timestamp) // deadline
+            )
+        );
+
+        // Confirm our closing balances, showing $1,000 spent and 0.53~ WETH received
+        assertEq(ERC20(USDC).balanceOf(treasury), 76979271562891);
+        assertEq(ERC20(WETH).balanceOf(treasury), 535966913395567553);
+
+        vm.stopPrank();
     }
 
     /**
      * Confirm that we can swap an approved amount of token with sufficient balance
      * and receive back the expected amount of WETH into the {Treasury}.
      */
-    function test_CanSwapToken() public {
+    function _test_CanSwapToken() public {
         vm.startPrank(treasury);
 
         // Approve $10,000 against the action contract
@@ -71,7 +102,7 @@ contract UniswapSellTokensForETHTest is FloorTest {
      * If we don't have sufficient approved balance when we request our swap, then
      * the transaction should be reverted.
      */
-    function test_CannotSwapTokenWithInsufficientBalance() public {
+    function _test_CannotSwapTokenWithInsufficientBalance() public {
         vm.startPrank(treasury);
 
         // Don't approve any tokens
@@ -94,7 +125,7 @@ contract UniswapSellTokensForETHTest is FloorTest {
      * If our swap generates an amount of WETH below the amount we specify, then we
      * expect the transaction to be reverted.
      */
-    function test_CannotSwapWithInsufficientAmountOutResponse() public {
+    function _test_CannotSwapWithInsufficientAmountOutResponse() public {
         vm.startPrank(treasury);
 
         // Approve $10,000 against the action contract
