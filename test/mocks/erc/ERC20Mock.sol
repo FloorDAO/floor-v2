723c8992bc37a48c284cf52d89387f5eb86abfbe
Tom Wade <twade7@gmail.com>
2023-07-25 11:48:22 +0100

    Handle decimals in sweep registration

--- test/mocks/erc/ERC20Mock.sol
+++ test/mocks/erc/ERC20Mock.sol
@@ -4,6 +4,8 @@ pragma solidity ^0.8.0;
 import {ERC20} from '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 
 contract ERC20Mock is ERC20 {
+    uint8 internal _decimals;
+
     constructor() ERC20('ERC20Mock', 'E20M') {}
 
     function mint(address account, uint amount) external {
@@ -12,5 +14,17 @@ contract ERC20Mock is ERC20 {
 
     function burn(address account, uint amount) external {
         _burn(account, amount);
+    }
+
+    function decimals() public view virtual override returns (uint8) {
+        return _decimals;
+    }
+
+    /**
+     * Allows the decimal accuracy of the token to be set. This should only be
+     * done straight after the token is created, and not after any further use.
+     */
+    function setDecimals(uint8 newDecimals) public {
+        _decimals = newDecimals;
     }
 }
