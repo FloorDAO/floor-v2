97914cb42abf11c31a9a3fed508fbd49e981fee8
Tom Wade <twade7@gmail.com>
2023-12-08 13:17:56 +0000

    Update Sepolia WETH address

--- script/deployment/DeploymentScript.sol
+++ script/deployment/DeploymentScript.sol
@@ -24,8 +24,8 @@ contract DeploymentScript is Script {
     /// Set our deployment WETH address
     // Mainnet: 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2
     // Goerli: 0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6
-    // Sepolia: 0x7b79995e5f793A07Bc00c21412e50Ecae098E7f9
-    address constant WETH = 0x7b79995e5f793A07Bc00c21412e50Ecae098E7f9;
+    // Sepolia: 0xfFf9976782d46CC05630D1f6eBAb18b2324d6B14
+    address constant WETH = 0xfFf9976782d46CC05630D1f6eBAb18b2324d6B14;
 
     /**
      * Ensures that a contract has been deployed already and returns the address of
