c1f47d5e774a5af323a0bec53a4b92a17451db2d
Tom Wade <twade7@gmail.com>
2023-12-15 09:38:53 +0000

    Helper script to deploy a distributed strategy

---
+++ script/deployment/000_DeployDistributedStrategy.s.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import 'forge-std/console.sol';
+
+import {CollectionRegistry} from '@floor/collections/CollectionRegistry.sol';
+import {DistributedRevenueStakingStrategy} from '@floor/strategies/DistributedRevenueStakingStrategy.sol';
+import {StrategyFactory} from '@floor/strategies/StrategyFactory.sol';
+
+import {IWETH} from '@floor-interfaces/tokens/WETH.sol';
+
+import {DeploymentScript} from '@floor-scripts/deployment/DeploymentScript.sol';
+
+
+contract DeployDistributedStrategy is DeploymentScript {
+
+    function run() external deployer {
+
+        CollectionRegistry collectionRegistry = CollectionRegistry(requireDeployment('CollectionRegistry'));
+        address epochManager = requireDeployment('EpochManager');
+        StrategyFactory strategyFactory = StrategyFactory(requireDeployment('StrategyFactory'));
+        address implementation = requireDeployment('DistributedRevenueStakingStrategy');
+        IWETH weth = IWETH(WETH);
+
+        // Add WETH as an approved collection
+        collectionRegistry.approveCollection(address(weth));
+
+        // Deploy our strategy
+        (uint strategyId, address strategy) = strategyFactory.deployStrategy(
+            'WETH Revenue',
+            implementation,
+            abi.encode(address(weth), 0.1 ether, epochManager),
+            address(weth)
+        );
+
+        // Wrap an ETH into WETH
+        weth.deposit{value: 1 ether}();
+
+        // Deposit WETH into the strategy
+        weth.approve(strategy, 1 ether);
+        DistributedRevenueStakingStrategy(strategy).depositErc20(1 ether);
+
+        console.log('WETH Revenue Distribution:');
+        console.log(strategyId);
+        console.log(strategy);
+        console.log('---');
+
+    }
+
+}
