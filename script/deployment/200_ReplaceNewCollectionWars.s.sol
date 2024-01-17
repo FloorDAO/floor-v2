c6056d7301b4ec1e9f6441f95b9df937877db5b3
Tom Wade <twade7@gmail.com>
2024-01-17 09:31:48 +0000

    Add epoch trigger permissions

--- script/deployment/200_ReplaceNewCollectionWars.s.sol
+++ script/deployment/200_ReplaceNewCollectionWars.s.sol
@@ -1,7 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-import 'forge-std/console.sol';
+import {IAuthorityControl} from '@floor-interfaces/authorities/AuthorityControl.sol';
+import {IAuthorityRegistry} from '@floor-interfaces/authorities/AuthorityRegistry.sol';
 
 import {VeFloorStaking} from '@floor/staking/VeFloorStaking.sol';
 import {FLOOR} from '@floor/tokens/Floor.sol';
@@ -25,6 +26,10 @@ contract ReplaceNewCollectionWars is DeploymentScript {
 
     function run() external deployer {
 
+        // Load and reference our live authority contracts
+        IAuthorityControl authorityControl = IAuthorityControl(requireDeployment('AuthorityControl'));
+        IAuthorityRegistry authorityRegistry = IAuthorityRegistry(requireDeployment('AuthorityRegistry'));
+
         epochManager = EpochManager(requireDeployment('EpochManager'));
         floor = FLOOR(requireDeployment('FloorToken'));
         staking = VeFloorStaking(requireDeployment('VeFloorStaking'));
@@ -45,12 +50,21 @@ contract ReplaceNewCollectionWars is DeploymentScript {
             requireDeployment('SweepWars')
         );
 
+        // Set our new register sweep end trigger
         epochManager.setEpochEndTrigger(address(registerSweep), true);
 
+        // Assign the new collection war contract
         staking.setVotingContracts(address(newCollectionWars), requireDeployment('SweepWars'));
         epochManager.setContracts(address(newCollectionWars), address(0));
+
+        // Set our epoch manager
         newCollectionWars.setEpochManager(address(epochManager));
         registerSweep.setEpochManager(address(epochManager));
+
+        // RegisterSweep needs a range of authorities
+        authorityRegistry.grantRole(authorityControl.EPOCH_TRIGGER(), address(registerSweep));
+        authorityRegistry.grantRole(authorityControl.TREASURY_MANAGER(), address(registerSweep));
+        authorityRegistry.grantRole(authorityControl.STRATEGY_MANAGER(), address(registerSweep));
 
         storeDeployment('NewCollectionWars', address(newCollectionWars));
         storeDeployment('RegisterSweepTrigger', address(registerSweep));
