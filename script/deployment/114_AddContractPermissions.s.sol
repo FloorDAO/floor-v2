b459ef720aca9b7a73242a8b9ee334b413ef497d
Tom Wade <twade7@gmail.com>
2024-01-31 15:07:44 +0000

    Remove excessive calls during deployment

--- script/deployment/114_AddContractPermissions.s.sol
+++ script/deployment/114_AddContractPermissions.s.sol
@@ -12,7 +12,6 @@ import {DeploymentScript} from '@floor-scripts/deployment/DeploymentScript.sol';
 contract AddContractPermissions is DeploymentScript {
     function run() external deployer {
         // Load and reference our live authority contracts
-        IAuthorityControl authorityControl = IAuthorityControl(requireDeployment('AuthorityControl'));
         IAuthorityRegistry authorityRegistry = IAuthorityRegistry(requireDeployment('AuthorityRegistry'));
 
         // Ensure we have required contracts already deployed that will receive roles
@@ -22,24 +21,36 @@ contract AddContractPermissions is DeploymentScript {
         address veFloorStaking = requireDeployment('VeFloorStaking');
         address vestingClaim = requireDeployment('VestingClaim');
 
+        // We define our roles here, rather than making lots of calls
+        bytes32 EPOCH_TRIGGER = keccak256('EpochTrigger');
+        bytes32 FLOOR_MANAGER = keccak256('FloorManager');
+        bytes32 GUARDIAN = keccak256('Guardian');
+        bytes32 TREASURY_MANAGER = keccak256('TreasuryManager');
+        bytes32 STRATEGY_MANAGER = keccak256('StrategyManager');
+        bytes32 VOTE_MANAGER = keccak256('VoteManager');
+
         // Allow our specified contracts to mint Floor tokens
-        authorityRegistry.grantRole(authorityControl.FLOOR_MANAGER(), migrateFloorToken);
-        authorityRegistry.grantRole(authorityControl.FLOOR_MANAGER(), treasury);
-        authorityRegistry.grantRole(authorityControl.FLOOR_MANAGER(), vestingClaim);
+        authorityRegistry.grantRole(FLOOR_MANAGER, migrateFloorToken);
+        authorityRegistry.grantRole(FLOOR_MANAGER, treasury);
+        authorityRegistry.grantRole(FLOOR_MANAGER, vestingClaim);
 
         // Allow specified contracts and wallets permission to interact with Treasury
-        authorityRegistry.grantRole(authorityControl.TREASURY_MANAGER(), treasury);
+        authorityRegistry.grantRole(TREASURY_MANAGER, treasury);
 
         // Allow specified contracts and wallets permission to create and pause Vaults
-        authorityRegistry.grantRole(authorityControl.STRATEGY_MANAGER(), treasury);
+        authorityRegistry.grantRole(STRATEGY_MANAGER, treasury);
 
         // RegisterSweep needs a range of authorities
-        authorityRegistry.grantRole(authorityControl.EPOCH_TRIGGER(), registerSweep);
-        authorityRegistry.grantRole(authorityControl.TREASURY_MANAGER(), registerSweep);
-        authorityRegistry.grantRole(authorityControl.STRATEGY_MANAGER(), registerSweep);
+        authorityRegistry.grantRole(EPOCH_TRIGGER, registerSweep);
+        authorityRegistry.grantRole(TREASURY_MANAGER, registerSweep);
+        authorityRegistry.grantRole(STRATEGY_MANAGER, registerSweep);
 
         // VeFloorStaking needs to be VOTE_MANAGER
-        authorityRegistry.grantRole(authorityControl.VOTE_MANAGER(), veFloorStaking);
+        authorityRegistry.grantRole(VOTE_MANAGER, veFloorStaking);
+
+        // Approve our guardian
+        // authorityRegistry.grantRole(authorityControl.GUARDIAN(), 0xA9d93A5cCa9c98512C8C56547866b1db09090326);  // mainnet
+        authorityRegistry.grantRole(GUARDIAN, 0x153c6D23fBB4D92335430e33882e575C5e81964A);  // sepolia
 
         // Transfer ownership of any required contracts
         // None currently required..
