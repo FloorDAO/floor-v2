c19b9e47ce805945449f5fd7994611afd42767f9
Tom Wade <twade7@gmail.com>
2024-01-31 15:06:02 +0000

    Set up Mainnet / Sepolia contract alternatives

--- script/deployment/000_ApproveCollection.s.sol
+++ script/deployment/000_ApproveCollection.s.sol
@@ -13,12 +13,18 @@ contract ApproveCollection is DeploymentScript {
         // Load and cast our Collection Registry
         CollectionRegistry collectionRegistry = CollectionRegistry(requireDeployment('CollectionRegistry'));
 
-        // Set up our approved collections
-        collectionRegistry.approveCollection(0x3d7E741B5E806303ADbE0706c827d3AcF0696516);
-        collectionRegistry.approveCollection(0xeA9aF8dBDdE2A8d3515C3B4E446eCd41afEdB1C6);
-        collectionRegistry.approveCollection(0xa807e2a221C6dAAFE1b4A3ED2dA5E8A53fDAf6BE);
+        // Mainnet collections
+        collectionRegistry.approveCollection(0x282BDD42f4eb70e7A9D9F40c8fEA0825B7f68C5D); // WRAPPED PUNK
+        collectionRegistry.approveCollection(0x521f9C7505005CFA19A8E5786a9c3c9c9F5e6f42); // WIZARD / Forgotten Runes
+        collectionRegistry.approveCollection(0x5Af0D9827E0c53E4799BB226655A1de152A425a5); // MILADY
+        collectionRegistry.approveCollection(0x059EDD72Cd353dF5106D2B9cC5ab83a52287aC3a); // SQUIGGLE
+        collectionRegistry.approveCollection(0x31385d3520bCED94f77AaE104b406994D8F2168C); // BGAN
+        collectionRegistry.approveCollection(0x57f1887a8BF19b14fC0dF6Fd9B2acc9Af147eA85); // ENS
+        collectionRegistry.approveCollection(0xe21EBCD28d37A67757B9Bc7b290f4C4928A430b1); // SAUDIS
+        collectionRegistry.approveCollection(0xD3D9ddd0CF0A5F0BFB8f7fcEAe075DF687eAEBaB); // REMILLIO
+        collectionRegistry.approveCollection(0x09f66a094a0070EBDdeFA192a33fa5d75b59D46b); // YAYO
 
-        // Approve the WETH token
-        collectionRegistry.approveCollection(DEPLOYMENT_WETH);
+        // Sepolia collections
+        // ..
     }
 }
--- script/deployment/115_ApproveSweepers.s.sol
+++ script/deployment/115_ApproveSweepers.s.sol
@@ -32,8 +32,10 @@ contract ApproveSweepers is DeploymentScript {
         } else {
             sudoswapSweeper = address(new SudoswapSweeper({
                 _treasury: payable(address(treasury)),
-                _pairFactory: payable(0x5bfE2ef160EaaAa4aFa89A8fa09775b6580162c9),
-                _gdaCurve: 0x2286e66cc3b3f15aE6d88164F618F98f1Ce21581
+                // _pairFactory: payable(0xA020d57aB0448Ef74115c112D18a9C231CC86000), // mainnet
+                // _gdaCurve: 0x1fD5876d4A3860Eb0159055a3b7Cb79fdFFf6B67 // mainnet
+                _pairFactory: payable(0x5bfE2ef160EaaAa4aFa89A8fa09775b6580162c9),  // sepolia
+                _gdaCurve: 0x2286e66cc3b3f15aE6d88164F618F98f1Ce21581  // sepolia
             }));
 
             treasury.approveSweeper(sudoswapSweeper, true);
